24dcc9652e3bde4aba63e8b358541de6623d8e6d
Aditi468 <adititripathy01@gmail.com>
2026-04-24 14:01:49 +0530

    Adjust footer-container style to include height

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const footer = `
     <footer>
       <footer>
-    <div class="footer-container" style="display: flex; justify-content: space-between; gap: 30px; flex-wrap: wrap;">
+    <div class="footer-container" style="display: flex; justify-content: space-between; gap: 30px; flex-wrap: wrap;height:100px">
 
         <!-- About Section -->
         <div class="footer-section" style="display: flex; align-items: flex-start; gap: 15px; max-width: 200px;">
